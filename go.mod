8fc5bcdc963ae8e9e5bab071d3414b290a2a88fd
Christian Stewart <christian@paral.in>
2018-11-02 22:48:08 -0700

    refactor: slim down library by removing listener, expose session
    Signed-off-by: Christian Stewart <christian@paral.in>

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/paralin/kcp-go-lite
+
+go 1.12
+
+require (
+	github.com/klauspost/cpuid v1.2.1 // indirect
+	github.com/klauspost/reedsolomon v1.9.2
+	github.com/pkg/errors v0.8.1
+	github.com/templexxx/cpufeat v0.0.0-20180724012125-cef66df7f161 // indirect
+	github.com/templexxx/xor v0.0.0-20181023030647-4e92f724b73b
+	github.com/tjfoc/gmsm v1.0.1
+	github.com/xtaci/lossyconn v0.0.0-20190602105132-8df528c0c9ae
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4
+	golang.org/x/net v0.0.0-20190628185345-da137c7871d7
+)
--- readloop.go
+++ readloop.go
@@ -1,9 +1,8 @@
 package kcp
 
 import (
-	"sync/atomic"
-
 	"github.com/pkg/errors"
+	"sync/atomic"
 )
 
 func (s *UDPSession) defaultReadLoop() {
@@ -26,22 +25,6 @@ func (s *UDPSession) defaultReadLoop() {
 			}
 		} else {
 			s.notifyReadError(errors.WithStack(err))
-			return
-		}
-	}
-}
-
-func (l *Listener) defaultMonitor() {
-	buf := make([]byte, mtuLimit)
-	for {
-		if n, from, err := l.conn.ReadFrom(buf); err == nil {
-			if n >= l.headerSize+IKCP_OVERHEAD {
-				l.packetInput(buf[:n], from)
-			} else {
-				atomic.AddUint64(&DefaultSnmp.InErrs, 1)
-			}
-		} else {
-			l.notifyReadError(errors.WithStack(err))
 			return
 		}
 	}
